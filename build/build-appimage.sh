c36ac9f54f2a0837724d27b9ef7ef3dbafcb2f9f
a-lameira <121145674+a-lameira@users.noreply.github.com>
2026-06-04 00:21:58 -0300

    Update build-appimage.sh

--- build/build-appimage.sh
+++ build/build-appimage.sh
@@ -22,20 +22,13 @@ docker run --rm -v "$PROJECT_ROOT:/work" ubuntu:rolling bash -c '
     # Install into AppDir
     DESTDIR=/work/AppDir ninja -C builddir install
 
-    # Copy desktop file and icon explicitly (linuxdeploy needs them)
-    cp data/dstx-gui.desktop /work/AppDir/usr/share/applications/
-    cp data/icons/dstx.svg /work/AppDir/usr/share/icons/hicolor/scalable/apps/
-
     # Download linuxdeploy and GTK plugin
     wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
     wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-gtk/releases/download/continuous/linuxdeploy-plugin-gtk-x86_64.AppImage
     chmod +x linuxdeploy*.AppImage
 
-    # Run with explicit desktop and icon file
-    ./linuxdeploy-x86_64.AppImage --appdir /work/AppDir \
-        --desktop-file /work/AppDir/usr/share/applications/dstx-gui.desktop \
-        --icon-file /work/AppDir/usr/share/icons/hicolor/scalable/apps/dstx.svg \
-        --plugin gtk --output appimage
+    # Run linuxdeploy – it will automatically find the .desktop file and icon inside AppDir
+    ./linuxdeploy-x86_64.AppImage --appdir /work/AppDir --plugin gtk --output appimage
 
     # Move result
     mv DSTX_GUI-*.AppImage /work/build/appimage/dstx-gui.AppImage
