d4d2337ccf72e66cbb141ca758204e3b23ded2ec
a-lameira <121145674+a-lameira@users.noreply.github.com>
2026-06-04 17:50:38 -0300

    Update build-appimage.sh

--- build/build-appimage.sh
+++ build/build-appimage.sh
@@ -10,12 +10,10 @@ echo "📦 Building AppImage (GUI only) using Debian Sid (libadwaita 1.9+, GTK 4
 docker run --rm -v "$PROJECT_ROOT:/work" debian:sid /bin/bash -c '
 set -ex
 
-# Instalar CA certificates e dpkg-dev (necessário para o plugin)
 apt-get update
 apt-get install -y --no-install-recommends ca-certificates dpkg-dev
 update-ca-certificates
 
-# Instalar dependências de build e bibliotecas de desenvolvimento
 apt-get install -y --no-install-recommends \
     meson ninja-build gcc g++ valac pkg-config wget gettext desktop-file-utils file \
     libgtk-4-dev libadwaita-1-dev libgee-0.8-dev libjson-glib-dev librsvg2-dev \
@@ -30,35 +28,44 @@ meson setup builddir --prefix=/usr
 ninja -C builddir
 DESTDIR=/work/AppDir ninja -C builddir install
 
-# Corrigir nome do ícone no arquivo .desktop
 sed -i "s/Icon=org.dstx.gui/Icon=dstx/" /work/AppDir/usr/share/applications/dstx-gui.desktop
-
 desktop-file-validate /work/AppDir/usr/share/applications/dstx-gui.desktop
 
-# Baixar linuxdeploy e o plugin GTK
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh
 chmod +x linuxdeploy-x86_64.AppImage linuxdeploy-plugin-gtk.sh
 
-# Extrair linuxdeploy (evitar FUSE)
 ./linuxdeploy-x86_64.AppImage --appimage-extract
 mv squashfs-root linuxdeploy-extracted
 
 export LINUXDEPLOY=/work/dstx-gui/linuxdeploy-extracted/AppRun
 
-# Primeira passagem: bibliotecas básicas
 $LINUXDEPLOY --appdir /work/AppDir --verbosity=1
 
-# Criar link simbólico para que o plugin encontre os módulos GTK
-ln -sf /usr/lib/x86_64-linux-gnu/gtk-4.0 /usr/lib/gtk-4.0
+# Diagnostic: check where GTK modules are
+echo "=== Checking GTK module locations ==="
+ls -la /usr/lib/x86_64-linux-gnu/ | grep gtk
+ls -la /usr/lib/x86_64-linux-gnu/gtk-4.0 || echo "Not found"
+ls -la /usr/lib/gtk-4.0 || echo "Not found"
 
-# Segunda passagem: plugin GTK
+# Create a symlink in the location the plugin expects
+# The plugin does: cd /usr/lib/x86_64-linux-gnu && cp -r gtk-4.0 ...
+# So we need that directory to exist. In Debian Sid, the real directory is /usr/lib/x86_64-linux-gnu/gtk-4.0
+# It should exist, but maybe it's a symlink itself? We'll create a hard directory if missing.
+if [ ! -d /usr/lib/x86_64-linux-gnu/gtk-4.0 ]; then
+    echo "Creating /usr/lib/x86_64-linux-gnu/gtk-4.0 symlink"
+    ln -sf /usr/lib/gtk-4.0 /usr/lib/x86_64-linux-gnu/gtk-4.0
+fi
+
+# Also ensure the modules are present in the system location (they are, but we copy them to AppDir manually)
+mkdir -p /work/AppDir/usr/lib
+cp -r /usr/lib/x86_64-linux-gnu/gtk-4.0 /work/AppDir/usr/lib/ || echo "Failed to copy GTK modules, will rely on plugin"
+
+# Run the plugin again (it may still attempt to copy, but source now exists)
 ./linuxdeploy-plugin-gtk.sh --appdir /work/AppDir
 
-# Criar AppImage final
 $LINUXDEPLOY --appdir /work/AppDir --output appimage --verbosity=1
 
-# Mover o AppImage gerado
 mv DSTX-*.AppImage /work/build/appimage/dstx-gui.AppImage
 '
 
