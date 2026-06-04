2e7fd42748c24cfb8cc6d98f82b323101614e9d9
a-lameira <121145674+a-lameira@users.noreply.github.com>
2026-06-04 17:42:06 -0300

    Update build-appimage.sh

--- build/build-appimage.sh
+++ build/build-appimage.sh
@@ -5,16 +5,22 @@ PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 OUTPUT_DIR="$PROJECT_ROOT/build/appimage"
 mkdir -p "$OUTPUT_DIR"
 
-echo "📦 Building AppImage (GUI only) using Fedora 41"
+echo "📦 Building AppImage (GUI only) using Debian Sid with workaround"
 
-docker run --rm -v "$PROJECT_ROOT:/work" fedora:41 /bin/bash -c '
+docker run --rm -v "$PROJECT_ROOT:/work" debian:sid /bin/bash -c '
 set -ex
 
-dnf install -y meson ninja-build gcc gcc-c++ vala pkg-config wget gettext desktop-file-utils file \
-    gtk4-devel libadwaita-devel libgee-devel json-glib-devel librsvg2-devel \
-    libxml2-devel glib2-devel cairo-devel pango-devel gdk-pixbuf2-devel \
-    wayland-devel libX11-devel libXrandr-devel libXrender-devel libXi-devel \
-    mesa-libGL-devel mesa-libEGL-devel vulkan-devel
+apt-get update
+apt-get install -y --no-install-recommends ca-certificates dpkg-dev
+update-ca-certificates
+
+apt-get install -y --no-install-recommends \
+    meson ninja-build gcc g++ valac pkg-config wget gettext desktop-file-utils file \
+    libgtk-4-dev libadwaita-1-dev libgee-0.8-dev libjson-glib-dev librsvg2-dev \
+    libglib2.0-dev libcairo2-dev libpango1.0-dev libgdk-pixbuf-2.0-dev \
+    libwayland-dev libx11-dev libxrandr-dev libxrender-dev libxi-dev \
+    libgl1-mesa-dev libgles2-mesa-dev libvulkan-dev \
+    flex bison gperf
 
 cd /work/dstx-gui
 meson setup builddir --prefix=/usr
@@ -22,7 +28,6 @@ ninja -C builddir
 DESTDIR=/work/AppDir ninja -C builddir install
 
 sed -i "s/Icon=org.dstx.gui/Icon=dstx/" /work/AppDir/usr/share/applications/dstx-gui.desktop
-
 desktop-file-validate /work/AppDir/usr/share/applications/dstx-gui.desktop
 
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
@@ -36,6 +41,8 @@ export LINUXDEPLOY=/work/dstx-gui/linuxdeploy-extracted/AppRun
 
 $LINUXDEPLOY --appdir /work/AppDir --verbosity=1
 
+# Workaround for missing gtk-4.0 directory: create it
+mkdir -p /work/AppDir/usr/lib/x86_64-linux-gnu/gtk-4.0
 ./linuxdeploy-plugin-gtk.sh --appdir /work/AppDir
 
 $LINUXDEPLOY --appdir /work/AppDir --output appimage --verbosity=1
