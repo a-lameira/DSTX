473cf6e19be11267fd04f63eae8a35221a91978a
a-lameira <121145674+a-lameira@users.noreply.github.com>
2026-06-04 17:28:19 -0300

    Update build-appimage.sh

--- build/build-appimage.sh
+++ build/build-appimage.sh
@@ -10,10 +10,12 @@ echo "📦 Building AppImage (GUI only) using Debian Sid"
 docker run --rm -v "$PROJECT_ROOT:/work" debian:sid /bin/bash -c '
 set -ex
 
+# Install CA certificates and dpkg-dev (for plugin architecture detection)
 apt-get update
-apt-get install -y --no-install-recommends ca-certificates
+apt-get install -y --no-install-recommends ca-certificates dpkg-dev
 update-ca-certificates
 
+# Install remaining build dependencies
 apt-get install -y --no-install-recommends \
     meson ninja-build gcc g++ valac pkg-config wget gettext desktop-file-utils file \
     libgtk-4-dev libadwaita-1-dev libgee-0.8-dev libjson-glib-dev librsvg2-dev \
@@ -27,25 +29,32 @@ meson setup builddir --prefix=/usr
 ninja -C builddir
 DESTDIR=/work/AppDir ninja -C builddir install
 
+# Fix desktop file icon name
 sed -i "s/Icon=org.dstx.gui/Icon=dstx/" /work/AppDir/usr/share/applications/dstx-gui.desktop
 
 desktop-file-validate /work/AppDir/usr/share/applications/dstx-gui.desktop
 
+# Download linuxdeploy and GTK plugin script
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh
 chmod +x linuxdeploy-x86_64.AppImage linuxdeploy-plugin-gtk.sh
 
+# Extract linuxdeploy (avoid FUSE)
 ./linuxdeploy-x86_64.AppImage --appimage-extract
 mv squashfs-root linuxdeploy-extracted
 
 export LINUXDEPLOY=/work/dstx-gui/linuxdeploy-extracted/AppRun
 
+# Deploy basic libraries
 $LINUXDEPLOY --appdir /work/AppDir --verbosity=1
 
+# Deploy GTK libraries using the plugin
 ./linuxdeploy-plugin-gtk.sh --appdir /work/AppDir
 
+# Create AppImage
 $LINUXDEPLOY --appdir /work/AppDir --output appimage --verbosity=1
 
+# Move generated AppImage
 mv DSTX-*.AppImage /work/build/appimage/dstx-gui.AppImage
 '
 
