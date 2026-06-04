97d093f92393ba07b3f06148731ca25bad44f686
a-lameira <121145674+a-lameira@users.noreply.github.com>
2026-06-04 00:56:19 -0300

    Update build-appimage.sh

--- build/build-appimage.sh
+++ build/build-appimage.sh
@@ -8,30 +8,46 @@ mkdir -p "$OUTPUT_DIR"
 echo "📦 Building AppImage (GUI only) using Ubuntu rolling"
 
 docker run --rm -v "$PROJECT_ROOT:/work" ubuntu:rolling /bin/bash -c '
-set -e
+set -ex  # enable debugging
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y meson ninja-build valac pkg-config wget gettext desktop-file-utils libgtk-4-dev libadwaita-1-dev libgee-0.8-dev libjson-glib-dev librsvg2-dev
+apt-get install -y meson ninja-build valac pkg-config wget gettext desktop-file-utils \
+    libgtk-4-dev libadwaita-1-dev libgee-0.8-dev libjson-glib-dev librsvg2-dev
 
 cd /work/dstx-gui
 meson setup builddir --prefix=/usr
 ninja -C builddir
 DESTDIR=/work/AppDir ninja -C builddir install
 
+# Validate desktop file
+echo "=== Desktop file content ==="
+cat /work/AppDir/usr/share/applications/dstx-gui.desktop
 desktop-file-validate /work/AppDir/usr/share/applications/dstx-gui.desktop
 
+# Download and extract linuxdeploy
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-gtk/releases/download/continuous/linuxdeploy-plugin-gtk-x86_64.AppImage
-chmod +x linuxdeploy*.AppImage
+chmod +x *.AppImage
 
 ./linuxdeploy-x86_64.AppImage --appimage-extract
 mv squashfs-root linuxdeploy-extracted
 ./linuxdeploy-plugin-gtk-x86_64.AppImage --appimage-extract
 mv squashfs-root plugin-extracted
 
-export LINUXDEPLOY_PLUGIN_PATH=/work/plugin-extracted/usr/lib/linuxdeploy/plugins
-/work/linuxdeploy-extracted/AppRun --appdir /work/AppDir --plugin gtk --output appimage --verbose
+# Plugin path (adjust according to extracted structure)
+export LINUXDEPLOY_PLUGIN_PATH=/work/plugin-extracted/usr/lib/linuxdeploy/plugins/
 
+# Run with explicit desktop and icon, and verbose output
+/work/linuxdeploy-extracted/AppRun \
+    --appdir /work/AppDir \
+    --desktop-file /work/AppDir/usr/share/applications/dstx-gui.desktop \
+    --icon-file /work/AppDir/usr/share/icons/hicolor/scalable/apps/dstx.svg \
+    --plugin gtk \
+    --output appimage \
+    --verbose
+
+# Move result
 mv DSTX_GUI-*.AppImage /work/build/appimage/dstx-gui.AppImage
 '
 
